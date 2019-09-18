1a969c61477e43869d4aa0985d1118305ecbde44
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-09-18 21:58:48 +0200

    add person component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Person from "./Person/Person";
+
 class App extends Component {
   render() {
-    // return (
-    //   // <div className="App">
-    //   //   <h1>Hi, I'm a React app</h1>
-    //   // </div>
-    // );
-    return React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "Does this work now?")
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app</h1>
+        <p>This is really working!</p>
+        <Person />
+      </div>
     );
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Does this work now?")
+    // );
   }
 }
 
