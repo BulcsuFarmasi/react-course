502f9d8605f118745059d1f3302543daa4c5a9e1
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-09-29 11:01:25 +0200

    outsourcing persons list rendering logic to Persons

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import classes from "./App.css";
-import Person from "../components/Persons/Person/Person";
+import Persons from "../components/Persons/Persons";
 
 class App extends Component {
   state = {
@@ -23,8 +23,6 @@ class App extends Component {
     // const person = Object.assign({}, this.state.persons[personIndex]);
 
     person.name = event.target.value;
-
-    console.log(person);
 
     const persons = [...this.state.persons];
 
@@ -54,16 +52,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                name={person.name}
-                age={person.age}
-                click={() => this.deletePersonHandler(index)}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+          />
         </div>
       );
       btnClass = classes.Red;
