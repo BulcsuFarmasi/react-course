1b91d6f9756e19ecd4e6527213dd92ad72f857a6
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-10-03 21:05:50 +0200

    add extra checks to shouldComponentUpdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -14,12 +14,15 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log("[Persons.js] shouldComponentUpdate");
-    // if (nextProps.persons !== this.props.persons) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
-    return true;
+    if (
+      nextProps.persons !== this.props.persons ||
+      nextProps.changed !== this.props.changed ||
+      nextProps.changed !== this.props.changed
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
