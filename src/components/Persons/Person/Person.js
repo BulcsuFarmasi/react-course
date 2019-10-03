502490bda2da0f162bf97fad105bc134ce465f99
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-10-03 21:31:13 +0200

    using adjecent elements with Aux component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
+import Auxiliary from "../../../hoc/Auxiliary";
 import classes from "./Person.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
-    return [
-      <p key="i1" onClick={this.props.clicked}>
-        I'm {this.props.name} and {this.props.age} years old!
-      </p>,
-      <p key="i2">{this.props.children}</p>,
-      <input
-        key="i3"
-        type="text"
-        value={this.props.name}
-        onChange={this.props.changed}
-      />
-    ];
+    return (
+      <Auxiliary>
+        <p onClick={this.props.clicked}>
+          I'm {this.props.name} and {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          value={this.props.name}
+          onChange={this.props.changed}
+        />
+      </Auxiliary>
+    );
   }
 }
 
