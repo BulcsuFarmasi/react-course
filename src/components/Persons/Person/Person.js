806491a8ed4fb6f01e0bd989bbed2b7dce7b0abf
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-10-03 21:21:32 +0200

    using adjecent elements as array

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,19 +4,18 @@ import classes from "./Person.css";
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.clicked}>
-          I'm {this.props.name} and {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          value={this.props.name}
-          onChange={this.props.changed}
-        />
-      </div>
-    );
+    return [
+      <p key="i1" onClick={this.props.clicked}>
+        I'm {this.props.name} and {this.props.age} years old!
+      </p>,
+      <p key="i2">{this.props.children}</p>,
+      <input
+        key="i3"
+        type="text"
+        value={this.props.name}
+        onChange={this.props.changed}
+      />
+    ];
   }
 }
 
